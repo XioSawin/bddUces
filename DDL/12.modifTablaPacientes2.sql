f25648bf29f9dbe569049f73fab363305c3f9512
XioSawin <x.r.sawin@gmail.com>
2018-09-17 20:49:04 -0300

    modificar tablas con id

---
+++ DDL/12.modifTablaPacientes2.sql
@@ -0,0 +1,8 @@
+DROP TABLE Pacientes
+
+create table Pacientes (
+	nombre varchar (255),
+	email varchar (80),
+	telefono varchar (40),
+	id int identity (1,1)
+)
---
+++ DDL/14.modifTablaServicio2.sql
@@ -0,0 +1,7 @@
+DROP TABLE Servicio
+
+create table Servicio (
+	nombre varchar (50),
+	tipo char (2),
+	id int identity (1,1)
+)
