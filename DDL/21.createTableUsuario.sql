b2bda4f2ff281d6ffbac9bb383f1cfd1cbbe6f19
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-10-07 23:23:52 -0300

    Tables fase 2

---
+++ DDL/21.createTableUsuario.sql
@@ -0,0 +1,5 @@
+create table usuario (
+	id int identity (1,1),
+	cantidad_por_pagina int,
+	nro_pagina int
+	)
---
+++ DDL/22.createTableActividad.sql
@@ -0,0 +1,6 @@
+create table actividad (
+	id int identity (1,1),
+	cantidad_por_pagina int,
+	nro_pagina int,
+	tipo_actividad char(1)
+	)
