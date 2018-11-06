4ca07f0a7026fbef6ce70dd1faee202e47d81160
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-11-06 10:21:23 -0300

    Update 08.modifTablePacientes.sql

--- DDL/08.modifTablePacientes.sql
+++ DDL/08.modifTablePacientes.sql
@@ -1,14 +1,3 @@
 ALTER TABLE Pacientes
 	ADD id int
 
-UPDATE Pacientes
-	set id=1
-	WHERE nombre='Colleen Wolfe'
-
-UPDATE Pacientes
-	set id=2
-	WHERE nombre='John Gonzalez'
-
-UPDATE Pacientes
-	set id=3
-	WHERE nombre='Erica Tamposi'
