8777ff416578d89d7497fcb0dab33bbc29ae0ce9
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-11-06 10:35:43 -0300

    Update 01.procedureAltaServicio.sql

--- Desarrollo/01.procedureAltaServicio.sql
+++ Desarrollo/01.procedureAltaServicio.sql
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 create procedure altaServicio
 	@nombre varchar (50), @tipo char (2)
 AS
 	BEGIN
-		INSERT INTO Servicio (nombre, tipo, id) values (@nombre, @tipo, 1)
+		INSERT INTO Servicio (nombre, tipo) values (@nombre, @tipo)
 	END
-=======
-create procedure altaServicio
-	@nombre varchar (50), @tipo char (2)
-AS
-	BEGIN
-		INSERT INTO Servicio (nombre, tipo, id) values (@nombre, @tipo, 1)
-	END
->>>>>>> 0b51debd7d10ad2a705f2bc52853a614a81b9731
+
  
