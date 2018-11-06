c5e966be76cfdc9d606d6c99b0d0563532f5423c
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-11-06 10:35:30 -0300

    Update 02.procedureAltaProfesional.sql

--- Desarrollo/02.procedureAltaProfesional.sql
+++ Desarrollo/02.procedureAltaProfesional.sql
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 create procedure altaProfesional
 	@nombre varchar (255), @email varchar (80), @telefono varchar (40)
 AS
 	BEGIN
 		INSERT INTO Profesional (nombre, email, telefono, id) values (@nombre, @email, @telefono, 1)
 	END
-=======
-create procedure altaProfesional
-	@nombre varchar (255), @email varchar (80), @telefono varchar (40)
-AS
-	BEGIN
-		INSERT INTO Profesional (nombre, email, telefono, id) values (@nombre, @email, @telefono, 1)
-	END
->>>>>>> 0b51debd7d10ad2a705f2bc52853a614a81b9731
+
