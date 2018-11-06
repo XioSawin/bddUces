51ab2f65295694aa77bed0b7fc3aad0161dab4bb
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-11-06 10:21:10 -0300

    Update 07.modifTableProfesional.sql

--- DDL/07.modifTableProfesional.sql
+++ DDL/07.modifTableProfesional.sql
@@ -1,14 +1,2 @@
 ALTER TABLE Profesional
 	ADD id int
-
-UPDATE Profesional
-	set id=1
-	WHERE nombre='Odell Beckham'
-
-UPDATE Profesional
-	set id=2
-	WHERE nombre='Julian Edelman'
-
-UPDATE Profesional
-	set id=3
-	WHERE nombre='Monica Bing'
