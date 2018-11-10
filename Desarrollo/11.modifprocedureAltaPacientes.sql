9c4b694991149d7c899fd401dd164eeafbcc7fff
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-11-10 19:36:45 -0300

    Delete 11.modifprocedureAltaPacientes.sql

--- Desarrollo/11.modifprocedureAltaPacientes.sql
+++
@@ -1,6 +0,0 @@
-create procedure altaPacientes
-	@nombre varchar (255), @email varchar (80), @telefono varchar (40), @id int
-AS
-	BEGIN
-		INSERT INTO Pacientes (nombre, email, telefono, id) values (@nombre, @email, @telefono, @id)
-	END
