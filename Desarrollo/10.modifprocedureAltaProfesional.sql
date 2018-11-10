862d15dac4ffc1c4e53e5b3136e86ef2fe4a0050
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-11-10 19:36:37 -0300

    Delete 10.modifprocedureAltaProfesional.sql

--- Desarrollo/10.modifprocedureAltaProfesional.sql
+++
@@ -1,6 +0,0 @@
-create procedure altaProfesional
-	@nombre varchar (255), @email varchar (80), @telefono varchar (40), @id int
-AS
-	BEGIN
-		INSERT INTO Profesional (nombre, email, telefono, id) values (@nombre, @email, @telefono, @id)
-	END
