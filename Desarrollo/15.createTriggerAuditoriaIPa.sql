9111fde41ae15c2428139937d251d1cc3d105189
XioSawin <42756313+XioSawin@users.noreply.github.com>
2018-12-06 17:37:11 -0300

    Update 15.createTriggerAuditoriaIPa.sql

--- Desarrollo/15.createTriggerAuditoriaIPa.sql
+++ Desarrollo/15.createTriggerAuditoriaIPa.sql
@@ -5,6 +5,6 @@ AFTER
 INSERT
 	AS
 	BEGIN
-		INSERT INTO auditoria (cantidad_por_pagina, nro_pagina, tipo_actividad)
-		values ((SELECT count (*) FROM INSERTED), 1, 'I')
+		INSERT INTO auditoria (cantidad_por_pagina, nro_pagina, tipo_actividad, fecha)
+		values ((SELECT count (*) FROM INSERTED), 1, 'I', getdate())
 	END
